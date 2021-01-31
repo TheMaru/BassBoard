e4cdcad3eaf0a0b9079a55faf87a840188b338bf
Ben Thiede <mailtothemaru@gmail.com>
2021-01-31 02:12:33 +0100

    added material ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import { globalStyles } from './styles';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
+      <Head>
+        <link
+          rel="preload"
+          as="style"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </Head>
       <Component {...pageProps} />
       <style jsx global>
         {globalStyles}
